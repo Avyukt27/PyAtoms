21fd439c9966b83d73f554182eda0b13da54d304
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-07-31 15:22:58 +0000

    modified:   .gitignore 	new file:   assets/images/oxygen.png 	modified:   pyproject.toml 	modified:   src/main.py 	modified:   uv.lock

--- src/main.py
+++ src/main.py
@@ -2,33 +2,28 @@ import json
 import os
 
 import requests
-import trimesh
 
 with open("assets/PeriodicTable.json", "r") as file:
     data = json.load(file)
 
 
-def get_glb_model(elementName: str) -> str:
+def save_model_and_image(elementName: str) -> None:
     element = next(e for e in data["elements"] if e["name"] == elementName)
     model_path = os.path.join("assets/models/", f"{elementName.lower()}.glb")
+    image_path = os.path.join("assets/images/", f"{elementName.lower()}.png")
 
     if not os.path.exists(model_path):
         response = requests.get(element["bohr_model_3d"])
         with open(model_path, "wb") as model:
             model.write(response.content)
-
-    return model_path
+    if not os.path.exists(image_path):
+        response = requests.get(element["bohr_model_image"])
+        with open(image_path, "wb") as image:
+            image.write(response.content)
 
 
 def main() -> None:
-    oxygen_path = get_glb_model("Oxygen")
-    mesh = trimesh.load(oxygen_path)
-    if isinstance(mesh, trimesh.Trimesh):
-        scene = mesh.scene()
-    else:
-        scene = mesh
-    scene.background = [0, 0, 0, 255]  # type: ignore[attr-defined]
-    scene.show()
+    save_model_and_image("Oxygen")
 
 
 if __name__ == "__main__":
