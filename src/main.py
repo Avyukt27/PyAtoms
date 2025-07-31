5bc0bb8befc830f1e881836fa0682f9a6f37de10
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-07-31 13:48:10 +0000

    new file:   .gitignore 	new file:   .python-version 	new file:   README.md 	new file:   assets/PeriodicTable.json 	new file:   assets/models/oxygen.glb 	new file:   pyproject.toml 	new file:   src/main.py 	new file:   uv.lock

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+import requests
+import trimesh
+
+with open("assets/PeriodicTable.json", "r") as file:
+    data = json.load(file)
+
+
+def get_glb_model(elementName: str) -> str:
+    element = next(e for e in data["elements"] if e["name"] == elementName)
+    model_path = os.path.join("assets/models/", f"{elementName.lower()}.glb")
+
+    if not os.path.exists(model_path):
+        response = requests.get(element["bohr_model_3d"])
+        with open(model_path, "wb") as model:
+            model.write(response.content)
+
+    return model_path
+
+
+def main() -> None:
+    oxygen_path = get_glb_model("Oxygen")
+    mesh = trimesh.load(oxygen_path)
+    mesh.show()
+
+
+if __name__ == "__main__":
+    main()
